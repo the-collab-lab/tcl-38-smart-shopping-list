9699509a97c599c06e5eff04f55a4fc7254ad14a
stacie.taylor <stacie.taylor@zapier.com>
2021-12-30 12:16:27 -0800

    Add firebase hosting

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,14 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 // Initialize Firebase.
-// These details will need to be replaced with the project specific env vars at the start of each new cohort.
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID
+  apiKey: "AIzaSyArP4G18A1NSWOOjRfurnMpAFdgav-ea84",
+  authDomain: "tcl-38-smart-shopping-list.firebaseapp.com",
+  projectId: "tcl-38-smart-shopping-list",
+  storageBucket: "tcl-38-smart-shopping-list.appspot.com",
+  messagingSenderId: "899088164997",
+  appId: "1:899088164997:web:d84357d7df8a7b5a4be1ec"
 };
 
 export const app = initializeApp(firebaseConfig);
