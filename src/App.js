1290fb47d6feda7080898726b8b92ef46079605f
chriskorsak <chris@chriskorsak.com>
2022-01-11 13:58:11 -0800

    Refactor routing components and create route links.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import AddItem from './components/AddItem';
+import ItemList from './components/ItemList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route element={<ItemList />} path="/" exact />
+          <Route element={<AddItem />} path="/add-item" />
+        </Routes>
+        <Link to="/" exact>
+          Item List
+        </Link>
+        <Link to="/add-item">Add Item</Link>
+      </Router>
     </div>
   );
 }
