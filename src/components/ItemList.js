849c806b9527398a1139f4c49b52f69897a354e8
Lars A Brekken <lars.arne.brekken@gmail.com>
2022-01-19 16:12:46 -0800

    Clean up view list page

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,5 +1,36 @@
+import { useEffect, useState } from 'react';
+import { collection, getDocs } from '@firebase/firestore';
+import { db } from '../lib/firebase.js';
+
 const ItemList = () => {
-  return <h1>Item List</h1>;
+  const [itemList, setItemList] = useState([]);
+
+  useEffect(() => {
+    const getItems = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'lars-test-token'));
+
+        const snapshotDocs = [];
+        // loop through snapshot and push each doc to array
+        querySnapshot.forEach((doc) => snapshotDocs.push(doc.data()));
+        // set array into our ItemList state
+        setItemList(snapshotDocs);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getItems();
+  }, []);
+
+  return (
+    <>
+      <h1>Shopping List</h1>
+      <ul>
+        {itemList &&
+          itemList.map((item) => <li key={item.name}>{item.name}</li>)}
+      </ul>
+    </>
+  );
 };
 
 export default ItemList;
