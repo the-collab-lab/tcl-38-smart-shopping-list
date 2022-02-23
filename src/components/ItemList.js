51c7d2d9b7e298e34e62e6085f4360ce6b711cf1
chriskorsak <chris@chriskorsak.com>
2022-02-22 20:00:59 -0800

    Fix bug so UI updates when purchasing and deleting items after filtering.

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -57,7 +57,7 @@ const ItemList = () => {
       const name = cleanData(item.data.name);
       return name.includes(cleanData(query));
     });
-    setFilteredResults(results);
+    !query ? setFilteredResults('') : setFilteredResults(results);
   };
 
   useEffect(() => {
