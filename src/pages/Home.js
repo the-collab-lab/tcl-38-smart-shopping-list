536de173dead478260e5f439efd6cd65848d988f
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-02-27 17:06:00 -0500

    tweaked styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { getToken } from '@the-collab-lab/shopping-list-utils';
 import useFirebaseSnapshot from '../hooks/useFirebaseSnapshot.js';
 import { useNavigate } from 'react-router-dom';
 import { useToken } from '../context/TokenContext.js';
-import logo from '../assets/logogrey.png';
+import logoS from '../assets/logogreyS.png';
 import or from '../assets/orshade.png';
 
 const Home = () => {
@@ -39,17 +39,17 @@ const Home = () => {
   return (
     <>
       <img
-        src={logo}
+        src={logoS}
         alt="Logo: Welcome to Your Smart Shopping List"
-        className="max-w-screen max-h-72 mx-auto mr-3 -mt-12"
+        className="max-w-screen max-h-72 mx-auto  ml-3 -mt-2 -mb-8"
       />
-      <div className="w-4/5 border-1 border-solid border-white/50 -mt-20 mb-0 mx-auto">
+      <div className="w-4/5 mx-auto border-solid border-white/50 -mt-22 mb-0 ">
         <div className="flex text-center flex-col m-0.5 border-2 border-dotted border-[99%] border-white/50">
-          <button onClick={createToken} className="btn-primary mt-3">
+          <button onClick={createToken} className="btn-primary  mt-8">
             Create a new list
           </button>
-          <img src={or} alt="or" className="max-w-xl max-h-44 mx-auto -mt-6" />
-          <span className="text-2xl text-white/80 -mt-8">
+          <img src={or} alt="or" className="max-w-xl max-h-44 mx-auto -mt-3 " />
+          <span className="text-3xl text-white/80 -mt-8 uppercase tracking-wide">
             Join an <br /> existing shopping list <br /> by entering a
           </span>
 
@@ -59,7 +59,7 @@ const Home = () => {
             </label>
             <br />
             <input
-              className="btn-primary text-white/80 hover:border-transparent text-2xl w-4/5 m-auto"
+              className="btn-primary text-white/80 hover:border-transparent -mb-3 text-2xl w-4/5 m-auto"
               id="shared-token"
               type="text"
               name="shared-token"
@@ -71,7 +71,7 @@ const Home = () => {
             <button
               type="submit"
               onClick={getUserToken}
-              className="btn-primary uppercase mt-20 mb-4"
+              className="btn-primary uppercase mt-20 mb-8"
             >
               Join an existing list
             </button>
