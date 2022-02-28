ef33552158c5c0739564b61ad597a60dfa037a93
chriskorsak <chris@chriskorsak.com>
2022-02-28 13:37:27 -0800

    implement copy token to clipboard with button in nav component.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,23 @@ import { useToken } from '../context/TokenContext';
 export default function Nav() {
   const { hasToken } = useToken();
 
+  async function copyTextToClipboard(text) {
+    if ('clipboard' in navigator) {
+      return await navigator.clipboard.writeText(text);
+    } else {
+      return document.execCommand('copy', true, text);
+    }
+  }
+
+  // onClick handler function for the copy button
+  const handleCopyClick = () => {
+    // Asynchronously call copyTextToClipboard
+    copyTextToClipboard(hasToken)
+      .then(() => window.alert('Token copied to clipboard!'))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <nav>
       {' '}
@@ -11,6 +28,7 @@ export default function Nav() {
         <>
           <NavLink to="/list">Item List</NavLink>
           <NavLink to="/add-item">Add Item</NavLink>
+          <button onClick={handleCopyClick}>Share Token</button>
         </>
       )}
     </nav>
