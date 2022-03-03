b0db8d4ed8455d6cbb973ad93baf23567a0d5678
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-03-02 23:03:35 -0500

    frontis designed

--- src/components/Frontis.js
+++ src/components/Frontis.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logoS from '../assets/logogreyS.png';
 
 const Frontis = () => {
   return (
-    <div>
-      Frontis
-      <Link to="/home">home</Link>
-      <Link to="/how-to-use">how to use</Link>
-    </div>
+    <>
+      <img
+        src={logoS}
+        alt="Logo: Welcome to Your Smart Shopping List"
+        className="logo mt-[40%]"
+      />
+      <div className="frontis-rule "></div>
+
+      <Link
+        to="/how-to-use"
+        className="text-3xl text-white/80 uppercase tracking-wide no-underline mt-10 frontis"
+      >
+        don't know how to use me?
+      </Link>
+      <div className="frontis-rule"></div>
+      <Link
+        to="/home"
+        className="text-3xl text-white/80 uppercase tracking-wide no-underline frontis"
+      >
+        i'm good, let me in
+      </Link>
+    </>
   );
 };
 
