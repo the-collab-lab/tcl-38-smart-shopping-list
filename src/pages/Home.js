83bedc87dba34932dce2d0ef41ad984a499b13dd
Kristen Monnik <nolaviolinist@gmail.com>
2022-03-04 14:46:27 -0500

    add desktop breakpoints for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -45,43 +45,48 @@ const Home = () => {
       />
       <div className="outer-box">
         <div className="inner-box">
-          <button onClick={createToken} className="btn-primary  mt-8">
-            Create a new list
-          </button>
-          <img
-            src={or}
-            alt="or"
-            className="max-h-44 mx-auto -mt-3 -ml-7 scale-75 -mb-6"
-          />
-
-          <span className="text-3xl text-white/80 uppercase tracking-wide">
-            Join an <br />
-            existing shopping list <br /> by entering a
-          </span>
-
-          <form>
-            <label htmlFor="shared-token" className="hidden">
-              Share Token
-            </label>
-            <br />
-            <input
-              className="btn-primary text-white/80 hover:border-transparent -mb-3 text-2xl w-4/5 m-auto"
-              id="shared-token"
-              type="text"
-              name="shared-token"
-              value={userToken}
-              placeholder="three word token"
-              onChange={(e) => setUserToken(e.target.value)}
-            />{' '}
-            <br />
+          <div className="md:flex md:flex-col md:items-center md:mx-auto md:max-w-sm md:h-[90%] md:my-[2%] md:border-2 md:border-white/50 md:border-dotted md:border-t-transparent md:border-b-transparent">
             <button
-              type="submit"
-              onClick={getUserToken}
-              className="btn-secondary"
+              onClick={createToken}
+              className="btn-primary mt-8 md:w-44 md:mb-2"
             >
-              Join an existing list
+              Create a new list
             </button>
-          </form>
+            <img
+              src={or}
+              alt="or"
+              className="max-h-44 mx-auto -mt-3 -ml-7 scale-75 -mb-6 md:mr-0"
+            />
+
+            <span className="text-xl text-white/80 uppercase tracking-wide md:text-2xl">
+              Join an <br />
+              existing shopping list <br /> by entering a
+            </span>
+
+            <form>
+              <label htmlFor="shared-token" className="hidden">
+                Share Token
+              </label>
+              <br />
+              <input
+                className="btn-primary text-white/80 hover:border-transparent -mb-3 text-2xl w-4/5 m-auto md:w-72"
+                id="shared-token"
+                type="text"
+                name="shared-token"
+                value={userToken}
+                placeholder="three word token"
+                onChange={(e) => setUserToken(e.target.value)}
+              />{' '}
+              <br />
+              <button
+                type="submit"
+                onClick={getUserToken}
+                className="btn-secondary md:w-44"
+              >
+                Join an existing list
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </>
